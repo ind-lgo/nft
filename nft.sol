92da56b63cc2470fcdcd238f71dea4c736f42a45
leon2241 <86055143+leon2241@users.noreply.github.com>
2021-12-24 16:26:23 +0300

    Update nft.sol

--- nft.sol
+++ nft.sol
@@ -1,3 +1,4 @@
+
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -8,75 +9,46 @@ contract NFT is ERC721Enumerable, Ownable {
 
   string public baseURI;
   string public baseExtension = ".json";
-  string public notRevealedUri;
-  uint256 public cost = 0.07 ether;
-  uint256 public maxSupply = 1000;
+  uint256 public cost = 0.05 ether;
+  uint256 public maxSupply = 10000;
   uint256 public maxMintAmount = 20;
-  uint256 public nftPerAddressLimit = 3;
   bool public paused = false;
-  bool public revealed = false;
-  bool public onlyWhitelisted = true;
-  address[] public whitelistedAddresses;
-  mapping(address => uint256) public addressMintedBalance;
-
+  mapping(address => bool) public whitelisted;
 
   constructor(
     string memory _name,
     string memory _symbol,
-    string memory _initBaseURI,
-    string memory _initNotRevealedUri
+    string memory _initBaseURI
   ) ERC721(_name, _symbol) {
     setBaseURI(_initBaseURI);
-    setNotRevealedURI(_initNotRevealedUri);
+    mint(msg.sender, 20);
   }
 
   // internal
+
+  
   function _baseURI() internal view virtual override returns (string memory) {
     return baseURI;
   }
 
   // public
-  function mint(uint256 _mintAmount) public payable {
-    require(!paused, "the contract is paused");
+  function mint(address _to, uint256 _mintAmount) public payable {
     uint256 supply = totalSupply();
-    require(_mintAmount > 0, "need to mint at least 1 NFT");
-    require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
-    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+    require(!paused);
+    require(_mintAmount > 0);
+    require(_mintAmount <= maxMintAmount);
+    require(supply + _mintAmount <= maxSupply);
 
     if (msg.sender != owner()) {
-      
-            uint256 ownerMintedCount = addressMintedBalance[msg.sender];
-            require(ownerMintedCount + _mintAmount <= nftPerAddressLimit, "max NFT per address exceeded");
        
-        require(msg.value >= cost * _mintAmount, "insufficient funds");
+          require(msg.value >= cost * _mintAmount);
+        
     }
-            if(onlyWhitelisted == true) {
-            devMintTokensToAddresses
-            }
+
     for (uint256 i = 1; i <= _mintAmount; i++) {
-        addressMintedBalance[msg.sender]++;
-      _safeMint(msg.sender, supply + i);
+      _safeMint(_to, supply + i);
     }
   }
-  
-  function isWhitelisted(address _user) public view returns (bool) {
-    for (uint i = 0; i < whitelistedAddresses.length; i++) {
-      if (whitelistedAddresses[i] == _user) {
-          return true;
-      }
-    }
-    return false;
-  }
-  
-  
-function devMintTokensToAddresses(address[] memory _whitelistedAddresses) external {
-
-for (uint256 i; i < _whitelistedAddresses.length; i++) {
-
- _safeMint(msg.sender, supply + i);
-
-}
-}
 
   function walletOfOwner(address _owner)
     public
@@ -102,10 +74,6 @@ for (uint256 i; i < _whitelistedAddresses.length; i++) {
       _exists(tokenId),
       "ERC721Metadata: URI query for nonexistent token"
     );
-    
-    if(revealed == false) {
-        return notRevealedUri;
-    }
 
     string memory currentBaseURI = _baseURI();
     return bytes(currentBaseURI).length > 0
@@ -114,14 +82,6 @@ for (uint256 i; i < _whitelistedAddresses.length; i++) {
   }
 
   //only owner
-  function reveal() public onlyOwner {
-      revealed = true;
-  }
-  
-  function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
-    nftPerAddressLimit = _limit;
-  }
-  
   function setCost(uint256 _newCost) public onlyOwner {
     cost = _newCost;
   }
@@ -137,24 +97,19 @@ for (uint256 i; i < _whitelistedAddresses.length; i++) {
   function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
     baseExtension = _newBaseExtension;
   }
-  
-  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-    notRevealedUri = _notRevealedURI;
-  }
 
   function pause(bool _state) public onlyOwner {
     paused = _state;
   }
-  
-  function setOnlyWhitelisted(bool _state) public onlyOwner {
-    onlyWhitelisted = _state;
-  }
-  
-  function whitelistUsers(address[] calldata _users) public onlyOwner {
-    delete whitelistedAddresses;
-    whitelistedAddresses = _users;
+ 
+ function whitelistUser(address _user) public onlyOwner {
+    whitelisted[_user] = true;
   }
  
+  function removeWhitelistUser(address _user) public onlyOwner {
+    whitelisted[_user] = false;
+  }
+
   function withdraw() public payable onlyOwner {
     // This will pay HashLips 5% of the initial sale.
     // You can remove this if you want, or keep it in to support HashLips and his channel.
